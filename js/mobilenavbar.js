4e074c5eccaf7aef17a00ad469544e915b5d3be4
subinpratap <subinpratap@gmail.com>
2016-05-18 04:41:04 +0530

    right calendar added

--- js/mobilenavbar.js
+++ js/mobilenavbar.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 
 	$("#menu").click(function(e){
-	
-		$(".navbar").css({"margin-left":"0"});
-		$('#overlay').show();
+		$(".navbar").toggle().css({"margin-left":"0"});;
+		
 	});
 
+	$("#overlay").click(function(e){
+		$(".navbar").css({"margin-left":"-1000px"});
+	});
 	
 });
